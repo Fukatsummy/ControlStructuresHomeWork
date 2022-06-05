f2638819dc08113596685bc50500e1b749f47060
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-06-05 22:29:17 +0300

    FIBONACCI DONE

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
 
+//#define TASK_1
+//#define TASK_2
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef TASK_1
 	double a;
 	int s;
 	double n = 1;
@@ -16,4 +20,27 @@ void main()
 		n *= a;
 	}
 	cout << "Результат: " << n << endl;
+#endif // TASK_1
+
+
+#ifdef TASK_2
+	int a;
+	double n = 1;
+	cout << "Введите число: "; cin >> a;
+	for (int i = 1; i <= a; i++)
+	{
+		n = n * i;
+		cout << "Факториал числа: " << n << endl;
+	}
+	cout << endl;
+#endif // TASK_2
+
+	int n;
+	cout << "Введите предел ряда Фибоначчи: "; cin >> n;
+	int a = 0, b = 1, c = a + b;
+	for (; a < n; a = b, b = c, c = a + b)
+	{
+		cout << a << "\t";
+	}
+	cout << endl;
 }
